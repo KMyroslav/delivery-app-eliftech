9185747e4acde3a14fa3d9b9a273d17b3b6569de
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-07-20 00:11:24 +0300

    Adds SPA handling

--- server/app.js
+++ server/app.js
@@ -6,5 +6,10 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 module.exports = app
