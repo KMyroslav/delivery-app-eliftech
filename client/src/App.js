ac42c1c2b5302e236c6e2f578d8736a7d50026dc
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-07-20 23:21:44 +0300

    Setups shop fetching

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 
+import { useDispatch } from "react-redux";
 
+import { fetchShops } from "./redux/shops/shopsOperations";
 
 import ShopsPage from "./pages/ShopsPage";
 import Nav from "./components/Nav";
 import CartPage from "./pages/CartPage";
 
 function App() {
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    dispatch(fetchShops());
+  }, [dispatch]);
 
   return (
     <div>
