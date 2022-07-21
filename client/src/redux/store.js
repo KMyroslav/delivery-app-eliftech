0a5072fa2f1d0328026499cf9b652a082baebc26
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-07-21 16:36:27 +0300

    Adds local storage logic for cart

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit';
-import shopsReducer from './shops/shopsSlice';
-import cartSlice from './cart/cartSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+
+import shopsSlice from "./shops/shopsSlice";
+import cartSlice from "./cart/cartSlice";
+import userSlice from "./user/userSlice";
+import orderSlice from "./order/orderSlice";
+
+const persistConfig = {
+  key: "cart",
+  storage,
+};
 
 export const store = configureStore({
   reducer: {
-    shops: shopsReducer,
-    cart: cartSlice.reducer
+    shops: shopsSlice.reducer,
+    cart: persistReducer(persistConfig, cartSlice.reducer),
+    user: userSlice.reducer,
+    order: orderSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
