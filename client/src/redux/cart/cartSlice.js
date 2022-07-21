58ed2b89bc80a9bfb6f52d4a73b95d68053a7a1b
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-07-21 16:29:28 +0300

    Adds remove item from cart

--- client/src/redux/cart/cartSlice.js
+++ client/src/redux/cart/cartSlice.js
@@ -4,19 +4,36 @@ const cartSlice = createSlice({
   name: "cart",
   initialState: { items: [], totalPrice: 0 },
   reducers: {
-    addItem: (state, { payload }) => {
+    manageItem: (state, { payload }) => {
       const idx = [...state.items].findIndex((el) => el.id === payload.id);
       return idx === -1
         ? {
+            ...state,
             items: [...state.items, payload],
-            totalPrice: state.totalPrice + payload.price * payload.quantity,
+            totalPrice: Number(
+              state.totalPrice + payload.price * payload.quantity
+            ).toFixed(2),
           }
         : {
+            ...state,
             items: [...state.items].map((el, i) => (i === idx ? payload : el)),
-            totalPrice:
+            totalPrice: Number(
               state.totalPrice +
-              payload.price * (payload.quantity - state.items[idx].quantity),
+                payload.price * (payload.quantity - state.items[idx].quantity)
+            ).toFixed(2),
           };
+    },
+    removeItem: (state, { payload }) => {
+      const item = [...state.items].find((el) => el.id === payload.item.id);
+      if (!item) return { ...state };
+      const totalPrice = Number(
+        state.totalPrice - item.price * item.quantity
+      ).toFixed(2);
+      return {
+        ...state,
+        items: state.items.filter((el) => el.id !== item.id),
+        totalPrice,
+      };
     },
   },
 });
