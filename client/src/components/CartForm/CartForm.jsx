35bf9ff1b9fd9e5fd54e1b2de7fde9a88b88e237
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-07-22 01:12:42 +0300

    Moves submit button to form

--- client/src/components/CartForm/CartForm.jsx
+++ client/src/components/CartForm/CartForm.jsx
@@ -3,9 +3,18 @@ import { useDispatch, useSelector } from "react-redux";
 import userSlice from "../../redux/user/userSlice";
 import * as yup from "yup";
 
+import { placeOrder } from "redux/order/orderOperations";
+import getCart from "redux/cart/cartSelectors";
+
+import VerificationError from "./VerificationError";
+
+import styles from "./cartForm.module.scss";
+
 export default function CartForm() {
   const dispatch = useDispatch();
-  const _ = "NaN";
+
+  const { items, totalPrice } = useSelector(getCart);
+
   const validationsSchema = yup.object().shape({
     name: yup
       .string()
@@ -34,57 +43,82 @@ export default function CartForm() {
           phone: "",
           address: "",
         }}
+        onSubmit={(values) => {
+          dispatch(
+            placeOrder({
+              shop: "McDonalds",
+              user: values,
+              items,
+              totalPrice,
+            })
+          );
+          dispatch(
+            userSlice.actions.addUserData({
+              data: values,
+            })
+          );
+        }}
         validationSchema={validationsSchema}
       >
-        {({ values, isValid, dirty }) => (
-          <Form
-            className={_}
-            onBlur={() =>
-              dispatch(
-                userSlice.actions.addUserData({
-                  data: values,
-                  isValid: isValid && dirty,
-                })
-              )
-            }
-          >
+        <Form className={styles.form}>
+          <label className={styles.label}>
+            Name
             <Field
               autoComplete="on"
               name="name"
-              placeholder="Name"
-              className={_}
+              placeholder="Marcus Smith"
+              className={styles.input}
             />
-            <ErrorMessage name="name" component="p" />
-
+            <ErrorMessage
+              name="name"
+              component={VerificationError}
+              className={styles.error}
+            />
+          </label>
+          <label className={styles.label}>
+            Email
             <Field
               autoComplete="on"
               type="email"
               name="email"
-              placeholder="E-mail"
+              placeholder="MarcusSmith@coolmail.com"
               id="email"
-              className={_}
+              className={styles.input}
             />
-            <ErrorMessage name="email" component="p" />
-
+            <ErrorMessage name="email" component={VerificationError} />
+          </label>
+          <label className={styles.label}>
+            Phone
             <Field
               autoComplete="on"
               type="phone"
               name="phone"
               placeholder="0907773333"
-              className={_}
+              className={styles.input}
             />
-            <ErrorMessage name="phone" component="p" />
-
+            <ErrorMessage name="phone" component={VerificationError} />
+          </label>
+          <label className={styles.label}>
+            Address
             <Field
               autoComplete="on"
               type="string"
               name="address"
               placeholder="614 Main St, Falmouth, MA"
-              className={_}
+              className={styles.input}
             />
-            <ErrorMessage name="address" component="p" />
-          </Form>
-        )}
+            <ErrorMessage name="address" component={VerificationError} />
+          </label>
+          <div className={styles.checkOutWrapper}>
+            <p className={styles.totalPrice}>
+              Total price: {Number(totalPrice).toFixed(2) || "0.00"}$
+            </p>
+
+            <button className={styles.orderButton} type="submit">
+              Submit
+            </button>
+          </div>
+        </Form>
       </Formik>
     </>
   );
