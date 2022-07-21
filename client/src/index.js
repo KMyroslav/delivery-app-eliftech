8d1be74233bd48ca20925a6bb7cc54fbe38ec5ff
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-07-21 16:37:45 +0300

    Adds persistor for local storage

--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
-import axios from 'axios';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import reportWebVitals from "./reportWebVitals";
+import axios from "axios";
 
-const BASE_URL = 'https://delivery-app-eliftech-mk.herokuapp.com'
+import { persistor } from "./redux/store";
+import { store } from "./redux/store";
+import App from "./App";
+import "./index.css";
+
+// const BASE_URL = 'https://delivery-app-eliftech-mk.herokuapp.com'
+const BASE_URL = "http://localhost:8080";
 
 axios.defaults.baseURL = BASE_URL;
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading="null" persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
