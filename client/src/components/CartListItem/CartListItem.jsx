1e5c23ed40e565ee5ad320ad337ee3905a8c6b05
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-07-22 01:11:58 +0300

    Creates cart page styles

--- client/src/components/CartListItem/CartListItem.jsx
+++ client/src/components/CartListItem/CartListItem.jsx
@@ -1,33 +1,45 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import cartSlice from "../../redux/cart/cartSlice";
+import styles from "./cartListItem.module.scss";
 
 export default function CartListItem({ item }) {
   const dispatch = useDispatch();
   const [quantity, setQuantity] = useState(item.quantity);
 
   return (
-    <li>
-      <img src={item.img} alt={`${item.name}`} />
-      <p>{item.name}</p>
-      <p>Price: {Number(item.price * quantity).toFixed(2)}$</p>
-      <input
-        type="number"
-        name="quantity"
-        min="1"
-        max="25"
-        step="1"
-        value={quantity}
-        onChange={(e) => {
-          setQuantity(e.target.value);
-          dispatch(
-            cartSlice.actions.manageItem({ ...item, quantity: e.target.value })
-          );
-        }}
-      />
-      <button onClick={() => dispatch(cartSlice.actions.removeItem({ item }))}>
-        Remove
-      </button>
+    <li className={styles.listItem}>
+      <img src={item.img} alt={`${item.name}`} className={styles.img} />
+      <div className={styles.infoWrapper}>
+        <p className={styles.name}>{item.name}</p>
+        <p className={styles.price}>
+          Price: {Number(item.price * quantity).toFixed(2)}$
+        </p>
+        <input
+          className={styles.input}
+          type="number"
+          name="quantity"
+          min="1"
+          max="25"
+          step="1"
+          value={quantity}
+          onChange={(e) => {
+            setQuantity(e.target.value);
+            dispatch(
+              cartSlice.actions.manageItem({
+                ...item,
+                quantity: e.target.value,
+              })
+            );
+          }}
+        />
+        <button
+          className={styles.button}
+          onClick={() => dispatch(cartSlice.actions.removeItem({ item }))}
+        >
+          Remove
+        </button>
+      </div>
     </li>
   );
 }
--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -2,40 +2,26 @@ import getCart from "../redux/cart/cartSelectors";
 
 import { useDispatch, useSelector } from "react-redux";
 
-import { placeOrder } from "../redux/order/orderOperations";
-
 import CartListItem from "../components/CartListItem";
 import CartForm from "../components/CartForm";
 import getUser from "../redux/user/userSelectors";
 
+import styles from "./cartPage.module.scss";
+
 export default function CartPage() {
-  const dispatch = useDispatch();
-  const { items, totalPrice } = useSelector(getCart);
-  const { data: user, isValid } = useSelector(getUser);
+  const { items } = useSelector(getCart);
 
   return (
-    <>
+    <div className={styles.cartPageWrapper}>
       <CartForm />
-      <div>
-        <ul>
+
+      <div className={styles.cartListWrapper}>
+        <ul className={styles.cartList}>
           {items.map((el) => (
             <CartListItem item={el} key={el.id} />
           ))}
         </ul>
       </div>
-      <div>
-        <p>total price: {Number(totalPrice).toFixed(2)}$</p>
-        <button
-          disabled={!items || !isValid}
-          onClick={() => {
-            dispatch(
-              placeOrder({ shop: "McDonalds", user, items, totalPrice })
-            );
-          }}
-        >
-          Order!
-        </button>
-      </div>
-    </>
+    </div>
   );
 }
