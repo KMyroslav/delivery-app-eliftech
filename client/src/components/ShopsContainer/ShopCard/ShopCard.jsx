f45a65466bc111a4fad75425f6ffffdb4ab7d578
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-07-21 16:34:55 +0300

    Updates shop page components

--- client/src/components/ShopsContainer/ShopCard/ShopCard.jsx
+++ client/src/components/ShopsContainer/ShopCard/ShopCard.jsx
@@ -1,6 +1,8 @@
-export default function ShopCard(shopName, shopImg){
-    return <>
-        <img src={shopImg} alt="" />
-        <p>{shopName}</p>
+export default function ShopCard(shopName, shopImg) {
+  return (
+    <>
+      <img src={shopImg} alt={`${shopName} shop icon`} />
+      <p>{shopName}</p>
     </>
+  );
 }
--- client/src/components/ShopsContainer/ShopsContainer.jsx
+++ client/src/components/ShopsContainer/ShopsContainer.jsx
@@ -1,9 +1,25 @@
-export default function ShopsContainer({ shops, setShop }) {
+import { useDispatch, useSelector } from "react-redux";
+import getShops from "../../redux/shops/shopsSelectors";
+import getCart from "../../redux/cart/cartSelectors";
+import shopsSlice from "../../redux/shops/shopsSlice";
+
+export default function ShopsContainer() {
+  const { data } = useSelector(getShops);
+  const { items } = useSelector(getCart);
+  const dispatch = useDispatch();
+
   return (
     <div>
       <ul>
-        {shops.map((shop) => (
-          <li key={shop._id} onClick={() => setShop(shop)}>
+        {data.map((shop) => (
+          <li
+            key={shop._id}
+            onClick={() => {
+              !items.length
+                ? dispatch(shopsSlice.actions.setShop(shop))
+                : console.log("nope");
+            }}
+          >
             {/* <img src={shopImg} alt={shop.name} /> */}
             <p>{shop.name}</p>
           </li>
