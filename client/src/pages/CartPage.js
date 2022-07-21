011e369cf6963426e5400aee6fe3584954b9418b
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-07-22 01:57:18 +0300

    Update CartPage.js

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -1,10 +1,9 @@
 import getCart from "../redux/cart/cartSelectors";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 import CartListItem from "../components/CartListItem";
 import CartForm from "../components/CartForm";
-import getUser from "../redux/user/userSelectors";
 
 import styles from "./cartPage.module.scss";
 
