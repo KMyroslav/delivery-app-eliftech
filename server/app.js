1886e5eac1c04d78b2bcb1abbe244d1801e8bd11
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-07-21 16:37:26 +0300

    Adds order route

--- server/app.js
+++ server/app.js
@@ -1,33 +1,34 @@
-const path = require('path');
-const logger = require('morgan')
-const express = require('express')
+const path = require("path");
+const logger = require("morgan");
+const express = require("express");
 
-const cors = require('cors')
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-const {shopsRouter} = require('./routes')
+const { shopsRouter, ordersRouter } = require("./routes");
 
 app.use("/api/shops", shopsRouter);
+app.use("/api/order", ordersRouter);
 
-app.use('/api', (req, res) => {
-  res.status(404).json({ message: 'Not found' })
+app.use("/api", (req, res) => {
+  res.status(404).json({ message: "Not found" });
 });
 
-app.use('/api', (err, req, res, next) => {
-  const {status = 500, message = "Server error"} = err;
-  res.status(status).json({ message })
+app.use("/api", (err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
-module.exports = app
+module.exports = app;
