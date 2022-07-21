b9e14570e284817fe1aca02a335d37b304f1441f
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-07-21 16:35:38 +0300

    Assembles shopping cart page

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -1,14 +1,21 @@
 import getCart from "../redux/cart/cartSelectors";
 
-import { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+
+import { placeOrder } from "../redux/order/orderOperations";
+
 import CartListItem from "../components/CartListItem";
+import CartForm from "../components/CartForm";
+import getUser from "../redux/user/userSelectors";
 
 export default function CartPage() {
+  const dispatch = useDispatch();
   const { items, totalPrice } = useSelector(getCart);
+  const { data: user, isValid } = useSelector(getUser);
 
   return (
     <>
+      <CartForm />
       <div>
         <ul>
           {items.map((el) => (
@@ -17,8 +24,17 @@ export default function CartPage() {
         </ul>
       </div>
       <div>
-        <p>total price: {totalPrice}$</p>
-        <button>Order!</button>
+        <p>total price: {Number(totalPrice).toFixed(2)}$</p>
+        <button
+          disabled={!items || !isValid}
+          onClick={() => {
+            dispatch(
+              placeOrder({ shop: "McDonalds", user, items, totalPrice })
+            );
+          }}
+        >
+          Order!
+        </button>
       </div>
     </>
   );
