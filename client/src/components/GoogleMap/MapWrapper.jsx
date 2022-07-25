629562061e158a59e6adf48a9be21c375d070015
KMyroslav <79135705+KMyroslav@users.noreply.github.com>
2022-07-26 01:07:54 +0300

    Creates google map wrapper component

---
+++ client/src/components/GoogleMap/MapWrapper.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useLoadScript } from "@react-google-maps/api";
+import Map from "./Map";
+import { Bars } from "react-loader-spinner";
+
+function MapWrapper() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.GOOGLE_API_KEY || "",
+  });
+
+  const renderMap = () => <Map />;
+  return isLoaded ? (
+    renderMap()
+  ) : loadError ? (
+    "Cant connect to google maps, sorry."
+  ) : (
+    <Bars
+      height="30"
+      width="30"
+      color="#2196f3"
+      ariaLabel="loading-indicator"
+    />
+  );
+}
+
+export default React.memo(MapWrapper);
